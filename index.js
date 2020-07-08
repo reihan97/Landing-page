27b71be79825f2c4510042a1b836a52b0066c0e4
reihan97 <perreyhan96@yahoo.com>
2020-07-08 19:18:04 +0300

    Styling

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+// to connect the nav options to it's particular page
+//using innerHTML, onclick on the tab the context is changed 
+
+const headline = document.getElementById('headline'),
+      projects = document.getElementById('projects'),
+      contact = document.getElementById('contact');
