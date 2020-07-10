8e9d53f02097ae6e44492d64f1ceef206ceccca6
reihan97 <perreyhan96@yahoo.com>
2020-07-10 11:52:48 +0300

    contact me section
    Some fixes and changes

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+// show the headers when mouseover on the icon
+
+const linkedin = document.getElementById("linkedin"),
+        codepen = document.getElementById("codepen"),
+        github = document.getElementById("github"),
+        githubH = document.getElementById("github-h"),
+        codepenH = document.getElementById("codepen-h"),
+        linkedinH = document.getElementById("linkedin-h");
+
+function openNav() {
+    document.getElementById("contact-nav").style.width  = "100%";
+    document.getElementById("contact-nav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("contact-nav").style.width = "0%";
+    document.getElementById("contact-nav").style.display = "none";
+}
 
 
-// to connect the nav options to it's particular page
-//using innerHTML, onclick on the tab the context is changed 
 
-const headline = document.getElementById('headline'),
-      projects = document.getElementById('projects'),
-      contact = document.getElementById('contact');
+/*linkedin.addEventListener("mouseover", ()=>{
+    linkedinH.style.display = "block";
+} );
+
+linkedin.addEventListener("mouseout", ()=>{
+    linkedinH.style.display = "none";
+});
+
+codepen.addEventListener("mouseover", ()=>{
+    codepenH.style.display = "block";
+});
+
+codepen.addEventListener("mouseout", ()=>{
+    codepenH.style.display = "none";
+});
+
+github.addEventListener("mouseover", ()=>{
+    codepenH.style.display = "block";
+});
+
+github.addEventListener("mouseout", ()=>{
+    githubH.style.display = "none";
+});
+
+*/
+
+
